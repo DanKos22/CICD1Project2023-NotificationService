d9839318370b3474fceecc1a02039a5e9a58589a
DanKos22 <G00397054@atu.ie>
2024-01-08 19:05:17 +0000

    Started Microservice

---
+++ src/main/java/ie/atu/cicd1project2023notificationservice/Cicd1Project2023NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1project2023notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1Project2023NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1Project2023NotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/cicd1project2023notificationservice/Cicd1Project2023NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.cicd1project2023notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1Project2023NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
